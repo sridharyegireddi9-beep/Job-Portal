3a15b6762a062a2f689cf7546b1b4017c116fd83
sridhar yegireddi <sridharyegireddi9@gmail.com>
2026-06-05 11:45:22 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/applications", require("./routes/applicationRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 
 const PORT = process.env.PORT || 5000;
-
+ 
 app.listen(PORT, () => {
   console.log(`Server Running on Port ${PORT}`);
 });
